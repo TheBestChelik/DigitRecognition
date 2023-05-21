1a5fdcd4a5507675a5c458d51077298925670ca0
Eugene <64377485+Eugenerio@users.noreply.github.com>
2023-05-21 21:14:44 +0200

    Update digitRecogniser.py

--- digitRecogniser.py
+++ digitRecogniser.py
@@ -13,27 +13,27 @@ class Recognizer:
         prediction = self.model.predict(img, verbose = 0)
         return np.argmax(prediction)
 
-if __name__ == "__main__":
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train),(x_test, y_test) = mnist.load_data()
+# if __name__ == "__main__":
+#     mnist = tf.keras.datasets.mnist
+#     (x_train, y_train),(x_test, y_test) = mnist.load_data()
 
-    x_train = tf.keras.utils.normalize(x_train, axis= 1)
-    x_test = tf.keras.utils.normalize(x_test, axis= 1)
-    model = tf.keras.models.load_model("handwriten.model")
+#     x_train = tf.keras.utils.normalize(x_train, axis= 1)
+#     x_test = tf.keras.utils.normalize(x_test, axis= 1)
+#     model = tf.keras.models.load_model("handwriten.model")
 
-    print(model.evaluate(x_test, y_test))
-    i = 0
-    with open("digits/ans.txt", "r") as f:
-        ans = f.read()
+#     print(model.evaluate(x_test, y_test))
+#     i = 0
+#     with open("digits/ans.txt", "r") as f:
+#         ans = f.read()
 
 
 
-    while os.path.isfile(f"digits/{i}.png"):
-        img = cv2.imread(f"digits/{i}.png")[:,:,0]
+#     while os.path.isfile(f"digits/{i}.png"):
+#         img = cv2.imread(f"digits/{i}.png")[:,:,0]
         
 
-        img = np.invert(np.array([img]))
-        #img = tf.keras.utils.normalize(img, axis=1)
-        prediction = model.predict(img)
-        print(ans[i], " ==> ",np.argmax(prediction))
-        i+=1
+#         img = np.invert(np.array([img]))
+#         #img = tf.keras.utils.normalize(img, axis=1)
+#         prediction = model.predict(img)
+#         print(ans[i], " ==> ",np.argmax(prediction))
+#         i+=1
